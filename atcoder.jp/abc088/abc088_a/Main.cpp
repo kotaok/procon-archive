a61f64910d7cec1ee0e9b1f4e4f5b59454a68326
lemon_melon <kota813e@gmail.com>
2019-11-02 17:08:51 +0900

    [AC] abc088 abc088_a

---
+++ atcoder.jp/abc088/abc088_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N , A;
+  cin >> N >> A;
+  if (N % 500 <= A) {
+  	cout << "Yes" << endl;
+  }
+  else 
+    cout << "No" << endl;
+}
