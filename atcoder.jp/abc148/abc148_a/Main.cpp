22dce4f4f0c7cf373f30b4ccb92dc6703398c875
lemon_melon <kota813e@gmail.com>
2019-12-22 21:10:26 +0900

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    
+    if ((a+b) == 3) {
+        cout << 3 << endl;
+    }
+    else if ((a+b) == 4) {
+        cout << 2 << endl;
+    }
+    else if ((a+b) == 5) {
+        cout << 1 << endl;
+    }
+
+}
