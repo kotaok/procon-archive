55517627f1624665e4e2f51e567ff8601e2f25dd
lemon_melon <kota813e@gmail.com>
2019-12-22 20:18:10 +0900

    [AC] abs practice_1

---
+++ atcoder.jp/abs/practice_1/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  int y, z;
+  cin >> y >> z;
+  
+  string s;
+  cin >> s;
+  
+  cout << (x+y+z) << " " << s << endl;
+  return 0;
+}
+  
+  
