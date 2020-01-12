cc877586188c7f05be6c8b9a5747e7e06ed56318
lemon_melon <kota813e@gmail.com>
2020-01-12 09:17:35 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    cout << (N * N * N) << endl;
+}
