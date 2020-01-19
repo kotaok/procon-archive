54b037b24c05eef49fb121b8a46e2ab9328570cd
lemon_melon <kota813e@gmail.com>
2020-01-19 20:53:38 +0900

    [AC] abc152 abc152_a

---
+++ atcoder.jp/abc152/abc152_a/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    if (N == M) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+}   
