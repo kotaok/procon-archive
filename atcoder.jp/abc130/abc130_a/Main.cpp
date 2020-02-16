9310650020395f0db685893ca83288a0113bedef
lemon_melon <kota813e@gmail.com>
2020-02-16 20:54:29 +0900

    [AC] abc130 abc130_a

---
+++ atcoder.jp/abc130/abc130_a/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int x, a;
+    cin >> x >> a;
+    if(x<a){
+        cout<<"0";
+    }
+    else if(x>=a){
+        cout<<"10";
+    }
+}   
