ff8b8ac2f98be92b442c88190f61a538a516d00a
lemon_melon <kota813e@gmail.com>
2020-02-23 10:39:01 +0900

    [AC] abc137 abc137_b

---
+++ atcoder.jp/abc137/abc137_b/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+
+int main()
+{
+    int K,X;
+    cin>>K>>X;
+    for(int i = X - K+1;i<X + K;i++){
+        log(i);
+    }
+}
