b1b9568db66ad854b35bfa56bd1daf2937837178
lemon_melon <kota813e@gmail.com>
2020-03-02 20:15:23 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+
+int main()
+{
+    int a;
+    string s;
+    cin>>a>>s;
+    if(a>=3200){
+        log(s);
+    }
+    else{
+        cout<<"red";
+    }
+}
