695424798dabba4c87cb4e4e56191ca625c4c1ca
lemon_melon <kota813e@gmail.com>
2020-03-14 21:03:12 +0900

    [AC] panasonic2020 panasonic2020_a

---
+++ atcoder.jp/panasonic2020/panasonic2020_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+int main(){
+  int k;
+  cin>>k;
+  int l[] = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+  cout<<l[k-1];
+}
