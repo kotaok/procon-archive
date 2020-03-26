17ba78f8554f7d36c34b60533588b5f2fc883482
lemon_melon <kota813e@gmail.com>
2020-03-26 11:56:53 +0900

    [AC] abc127 abc127_a

---
+++ atcoder.jp/abc127/abc127_a/Main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <cmath>
+#include <numeric>
+#include <iomanip>
+
+
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+
+
+// <<fixed<<setprecision(6)<<
+
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+
+int Combination(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a>=13){
+    cout<<b;
+  }
+  else if (6<=a&&a<=12){
+    cout<<b/2;
+  }
+  else{
+    cout<<'0';
+  }
+}
