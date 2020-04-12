bd43e8fa899fb3949776c7df56affc725a17c46a
lemon_melon <kota813e@gmail.com>
2020-04-12 21:05:59 +0900

    [AC] abc162 abc162_b

---
+++ atcoder.jp/abc162/abc162_b/Main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <cmath>
+#include <numeric>
+#include <iomanip>
+
+
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+
+
+// <<fixed<<setprecision(6)<<
+
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+
+int main(){
+  lint n;
+  cin>>n;
+  lint count=0;
+  for(lint i=1;i<=n;i++){
+    if(i%15==0){
+
+    }
+    else if(i%3==0){
+
+    }
+    else if(i%5==0){
+
+    }
+    else{
+      count+=i;
+    }
+  }
+  cout<<count;
+}
