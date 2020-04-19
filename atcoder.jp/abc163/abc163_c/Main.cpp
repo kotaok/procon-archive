8c7de55e76b00435ab6f985b32bc602da0f34dc1
lemon_melon <kota813e@gmail.com>
2020-04-19 21:23:11 +0900

    [AC] abc163 abc163_c

---
+++ atcoder.jp/abc163/abc163_c/Main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <cmath>
+#include <numeric>
+#include <iomanip>
+
+
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+
+
+// <<fixed<<setprecision(6)<<
+
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+
+int main(){
+  int n;
+  cin>>n;
+  map<int,int> m;
+  for (int i = 0; i < n-1; i++){
+    int j;
+    cin>>j;
+    m[j]++;
+  }
+  for(int i=1;i<=n;i++){
+    cout<<m[i]<<endl;
+  }
+  
+}
