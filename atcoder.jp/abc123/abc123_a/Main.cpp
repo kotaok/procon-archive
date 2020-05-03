190302b9f0e15261ed91282e633736922cb438aa
lemon_melon <kota813e@gmail.com>
2020-05-03 09:01:47 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <cmath>
+#include <numeric>
+#include <iomanip>
+
+
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+
+
+// <<fixed<<setprecision(6)<<
+
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+
+int main(){
+  vector<int> l;
+  for(int i=0;i<5;i++){
+    int j;
+    cin>>j;
+    l.pb(j);
+  }
+  int k;
+  cin>>k;
+  bool is=true;
+  for(int i=4;i>0;i--){
+    for(int j=i-1;j>=0;j--){
+      if(l[i]-l[j]>k){
+        is=false;
+      }
+    }
+  }
+  if(is){
+    cout<<"Yay!";
+  }
+  else{
+    cout<<":(";
+  }
+}
