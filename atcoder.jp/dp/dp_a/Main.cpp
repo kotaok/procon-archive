6e9a86d6765948cfd35c097dd12a616ae57872f8
lemon_melon <kota813e@gmail.com>
2020-05-17 18:07:37 +0900

    [AC] dp dp_a

---
+++ atcoder.jp/dp/dp_a/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+  int n;
+  lint num=1000000000;
+  cin>>n;
+  int h[100010];
+  int dp[100010];
+  rep(i,n){
+    cin>>h[i];
+    dp[i]=num;
+  }
+  dp[0]=0;
+  for(int i=1;i<n;++i){
+    chmin(dp[i],dp[i-1]+abs(h[i]-h[i-1]));
+    if(i>1) chmin(dp[i],dp[i-2]+abs(h[i]-h[i-2]));
+  }
+  cout<<dp[n-1];
+}
