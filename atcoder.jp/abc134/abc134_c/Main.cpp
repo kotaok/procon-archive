314f4d98f8531642550d1b17bbff1562c2097411
lemon_melon <kota813e@gmail.com>
2020-05-20 09:34:07 +0900

    [AC] abc134 abc134_c

---
+++ atcoder.jp/abc134/abc134_c/Main.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<pair<int,int>> l;
+  rep(i,n){
+    pair<int,int> e;
+    cin>>e.first;
+    e.second=i;
+    l.pb(e);
+  }
+  sort(all(l));
+  reverse(all(l));
+  rep(i,n){
+    int num=0;
+    if(l[num].second!=i) cout<<l[num].first<<endl;
+    else cout<<l[num+1].first<<endl;
+  }
+
+}
