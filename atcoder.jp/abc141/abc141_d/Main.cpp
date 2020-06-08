381d39963bb6eedfa80d1c3b8f2723918afc46cb
lemon_melon <kota813e@gmail.com>
2020-06-08 18:18:42 +0900

    [AC] abc141 abc141_d

---
+++ atcoder.jp/abc141/abc141_d/Main.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+using Graph=vector<vector<int>>;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+  int n,m;
+  lint all=0;
+  cin>>n>>m;
+  priority_queue<int,vector<int>,less<int>> q;
+  rep(i,n){
+    int j;
+    cin>>j;
+    q.push(j);
+  }
+  rep(i,m){
+    int j=q.top();
+    q.pop();
+    j/=2;
+    q.push(j);
+  }
+  rep(i,n){
+    int j=q.top();
+    q.pop();
+    all+=j;
+  }
+  cout<<all<<endl;
+
+  
+
+}
