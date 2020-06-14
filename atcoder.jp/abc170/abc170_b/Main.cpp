482481e9adef96f3fbc30d2d7d3192f3750c3fe0
lemon_melon <kota813e@gmail.com>
2020-06-14 21:22:55 +0900

    [AC] abc170 abc170_b

---
+++ atcoder.jp/abc170/abc170_b/Main.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+using Graph=vector<vector<int>>;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+  int x,y;
+  bool is=false;
+  cin>>x>>y;
+
+  for(int i=0;i<=100;i++){
+    for(int j=0;j<=100;j++){
+      if(i*2+j*4==y&&i+j==x&&i+j<=x){
+        is=true;
+      }
+    }
+  }
+
+  if(is){
+    cout<<"Yes"<<endl;
+  }
+  else{
+    cout<<"No"<<endl;
+  }
+}
