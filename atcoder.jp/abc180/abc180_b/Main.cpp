7e937d618138853ab9af00ff28cd634ff3e895fd
lemon_melon <kota813e@gmail.com>
2020-11-02 08:42:23 +0900

    [AC] abc180 abc180_b

---
+++ atcoder.jp/abc180/abc180_b/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define uni(x) (x).erase(unique(x.begin(),x.end()),x.end())
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+using Graph=vector<vector<int>>;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+  lint n;cin>>n;
+  lint a,b,c;
+  a=0;b=0;c=0;
+  rep(i,n){
+    lint j;cin>>j;
+    a+=abs(j);
+    b+=pow(j,2);
+    c=max(c,abs(j));
+  }
+  long double b1;
+  b1=sqrt(b);
+  cout<<fixed<<setprecision(10)<<a<<endl<<b1<<endl<<c<<endl;
+
+}
