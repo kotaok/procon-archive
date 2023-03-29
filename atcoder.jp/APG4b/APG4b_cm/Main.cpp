c65aeb2d6a7ce7b095f7f4d06e01b2388e70b8ed
lemon_melon <kota813e@gmail.com>
2023-03-29 19:11:30 +0900

    [AC] APG4b APG4b_cm

---
+++ atcoder.jp/APG4b/APG4b_cm/Main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout<<"A:";
+  for (int i = 0; i < A; i++)
+  {
+    cout<<"]";
+  }
+  cout<<endl;
+  cout<<"B:";
+  for (int i = 0; i < B; i++)
+  {
+    cout<<"]";
+  }
+  cout<<endl;
+  
+  
+
+  // ここにプログラムを追記
+}
