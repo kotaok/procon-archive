2e8189a80c0533829df2e2848b075ed9895855da
lemon_melon <kota813e@gmail.com>
2023-04-13 18:05:13 +0900

    [AC] abc295 abc295_c

---
+++ atcoder.jp/abc295/abc295_c/Main.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define log(x) cout<<x<<endl
+#define logfixed(x) cout<<fixed<<setprecision(10)<<x<<endl;
+#define pb push_back
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+typedef long long lint;
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const long long INF = 1LL << 60;
+const double PIE = 3.14159265358979;
+using Graph=vector<vector<int>>;
+
+int gcd(lint x, lint y){
+  return y==0 ? x:gcd(y,x % y);
+}
+
+int digsum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res += n%10;
+    n /= 10;
+  }
+  return res;
+}
+
+int dignum(int n) {
+  int res = 0;
+  while(n > 0) {
+    res++;
+    n /= 10;
+  }
+  return res;
+}
+
+int stringcount(string s, char c) {
+  return count(s.cbegin(), s.cend(), c);
+}
+
+int lcm(lint x, lint y){
+  return x/ gcd(x, y) * y;
+}
+
+int combi(int n, int r){
+  int num = 1;
+  for(int i = 1; i <= r; i++){
+    num = num * (n - i + 1) / i;
+  }
+  return num;
+}
+
+int main(){
+    int n;
+    lint count=0;
+    set<int> s;
+    map<int,int> m;
+    cin>>n;
+    for(int i=0;i<n;i++){
+      int j;
+      cin>>j;
+      s.insert(j);
+      m[j]++;
+    }
+    for(auto itr=s.begin();itr != s.end();++itr){
+      int t=*itr;
+      if(m[t]%2==0){
+        count+=m[t]/2;
+      }else {
+        count+=(m[t]-1)/2;
+
+      }
+    }
+    cout<<count<<endl;
+
+}
+    
